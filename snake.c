8e2a0e79979dbe771ba28a2d9747c78fafd53e38
Matheus Cazé <mvco@cesar.school>
2024-05-10 09:47:31 -0300

    (inicio) bibliotecas e variáveis

--- snake.c
+++ snake.c
@@ -4,9 +4,7 @@
 #include <conio.h>
 #include <time.h>
 
-#define MAX_RECORD 5
-
 int cobra[300][2]; //matriz do corpinho da cobra
-int maca[2]; //matriz da maçã (ou akuma no mi num futuro distante)
+int maca[2]; //matriz da maçã
 int pontos = 1; //pontuação do player
 int cx = 2, cy = 2; //coordenadas x e y
